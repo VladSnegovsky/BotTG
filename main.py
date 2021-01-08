fc806e9767142133de0176e4beeefe2371e89d8f
Snegovsky <nik.snegovskyvlad@gmail.com>
2021-01-08 22:18:00 +0200

    make it better

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.answer("Hi!")
+    await message.answer("Hi!!!")
 
 @dp.message_handler(content_types=["text"])
 async def answered(message):
